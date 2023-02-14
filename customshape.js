515920fc4aca9f0d5bbdeba9339b9f896234d752
evansoucy17 <124187275+evansoucy17@users.noreply.github.com>
2023-02-14 16:09:55 -0500

    Update customshape.js

--- customshape.js
+++ customshape.js
@@ -10,14 +10,23 @@ function draw() {
 
   beginShape(); //trying to make a nonperfect octogon, I dont want all the side to be the same length
   fill("dodgerblue");
+  //point 1
   vertex(100, 100);
+   //point 2
   vertex(100, 200);
+   //point 3
   vertex(150, 300);
+   //point 4
   vertex(250, 300);
+   //point 5
   vertex(300, 200);
+   //point 6
   vertex(300, 100);
+   //point 7
   vertex(250, 50);
+   //point 8
   vertex(150, 50);
+   //point 9
   vertex(100, 100);
   endShape(close); 
 }
